9d76f3b94b681f2223e4d2f2619e7fd007696be0
lucabaroni <loocabaroni@gmail.com>
2024-09-24 10:39:22 +0200

    update export single

--- export/export_single.sh
+++ export/export_single.sh
@@ -5,10 +5,8 @@
 #SBATCH --ntasks=4  # Requesting 16 processors
 #SBATCH --nodes=1
 #SBATCH --hint=nomultithread
-
-
 #SBATCH --exclude=w[1-2,8-12]
 
 
 # Call the merged Python script with the passed argument
-python /CSNG/baroni/mozaik-models/export/export.py $1 $2
+python /CSNG/baroni/mozaik-models/export/export.py $1 $2 $3
